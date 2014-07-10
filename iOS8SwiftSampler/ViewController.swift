065ac311c69215ef794b6cc9092af47652b87bb6
Karthik Prabhu <karthikprabhu.alagu@cognizant.com>
2014-07-10 15:43:56 +0530

    initial commit

---
+++ iOS8SwiftSampler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS8SwiftSampler
+//
+//  Created by Karthik Prabhu Alagu on 10/07/14.
+//  Copyright (c) 2014 Cognizant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
