028e04612d8f95212661d67748b53b6207b1d41e
Karthik Prabhu <karthikprabhu.alagu@cognizant.com>
2014-08-14 13:02:11 +0530

    completed UIActivityindicatorView

---
+++ iOS8SwiftSampler/KPActivityIndicatorViewController.swift
@@ -0,0 +1,55 @@
+//
+//  KPActivityIndicator.swift
+//  iOS8SwiftSampler
+//
+//  Created by Karthik Prabhu Alagu on 10/07/14.
+//  Copyright (c) 2014 Cognizant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class KPActivityIndicatorViewController:UIViewController
+{
+    var activityIndicator :UIActivityIndicatorView;
+    override func viewDidLoad()
+    {
+        super.viewDidLoad();
+    }
+    init(coder aDecoder: NSCoder!)
+    {
+         activityIndicator = UIActivityIndicatorView(frame: CGRectMake(0,0, 50, 50)) as UIActivityIndicatorView;
+        super.init(coder: aDecoder);
+    }
+    @IBAction
+    func buttonClicked(button:UIButton)
+    {
+        if(!button.selected)
+        {
+            button.selected = true;
+            button.setTitle("Stop", forState: UIControlState.Normal);
+            startActivityIndicator();
+        }
+        else
+        {
+            button.selected = false;
+            button.setTitle("Start", forState: UIControlState.Normal);
+            stopActivityIndicator();
+        }        
+    }
+    
+    func startActivityIndicator()
+    {
+        activityIndicator.center = self.view.center;
+        activityIndicator.hidesWhenStopped = true;
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray;
+        self.view.addSubview(activityIndicator);
+        activityIndicator.startAnimating();
+    }
+    
+    func stopActivityIndicator()
+    {
+        activityIndicator.stopAnimating();
+    }
+}
+
--- iOS8SwiftSampler/ViewController.swift
+++ iOS8SwiftSampler/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    var tableViewData:[String] = ["UIActivityIndicator","UIAlertView","UIActionSheet","UIButton"];
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
-    }
+           }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning();
         // Dispose of any resources that can be recreated.
     }
+    
 
+    
+    //UItableView datasource 
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return self.tableViewData.count;
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell : UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "KPHeaderCell");
+        cell.textLabel.text = self.tableViewData[indexPath.row];
+        return cell;
+     }
+    func tableView(tableView: UITableView!, titleForHeaderInSection section: Int) -> String!
+    {
+        return "iOS8 Swift programming sample";
+    }
+    //UItableView delegates
+    func tableView(tableView: UITableView!, heightForRowAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
+        return 44;
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        println("SELECTED INDEX \(indexPath.row)");
+        switch (indexPath.row) {
+        case 0:
+                let secondViewController = self.storyboard.instantiateViewControllerWithIdentifier("KPActivityIndicatorViewController") as KPActivityIndicatorViewController;
+                self.navigationController.pushViewController(secondViewController, animated: true);
+            break;
+            
+        default:
+            break;
+        }
+    }
 
 }
 
